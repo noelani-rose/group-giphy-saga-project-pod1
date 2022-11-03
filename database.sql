15ae63ccb4551d1742b441b19b9c31035ef4fca5
Noelani West <noelani.rose14@gmail.com>
2022-11-03 10:38:35 -0500

    update database, initial commit

--- database.sql
+++ database.sql
@@ -1,14 +1,22 @@
-CREATE DATABASE "giphy_search_favorites";
-
--- You'll need a table for storing each giphy image favorite
--- Each favorite image can be assigned 1 of the following categories as a Foreign Key
-
--- Category table
 CREATE TABLE "category" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR (100) NOT NULL
 );
 
--- Default categories. You may change them :)
+
 INSERT INTO "category" ("name")
-VALUES ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
+VALUES ('funny'), ('cute'), ('nsfw'), ('mood');
+
+CREATE TABLE "favorites" (
+	"id" SERIAL PRIMARY KEY,
+	"url" VARCHAR,
+	"category" VARCHAR(50)
+);
+
+CREATE TABLE "favorites_category" (
+	"id" SERIAL PRIMARY KEY,
+	"cat_id" INT REFERENCES "category",
+	"fav_id" INT REFERENCES "favorites"
+);
+
+
