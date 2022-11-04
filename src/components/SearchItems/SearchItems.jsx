4325b35cc1417ebb1d4df69eb825d9b44b5ebfac
Noelani West <noelani.rose14@gmail.com>
2022-11-04 12:09:29 -0500

    render proper url on search

--- src/components/SearchItems/SearchItems.jsx
+++ src/components/SearchItems/SearchItems.jsx
@@ -1,16 +1,21 @@
 import Checkbox from "@mui/material/Checkbox";
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
+// import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+// import BookmarkIcon from '@mui/icons-material/Bookmark';
 
 
 function SearchItems ({gif}) {
+    const label = { inputProps: { 'aria-label' : 'Checkbox demo' } }
     console.log('the gif from search list is', gif)
     return (
         <>
-        <h3>Here are the search items...</h3>
-        <img src = {gif.url} />
+        <img src = {gif.images.downsized.url} />
+        <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+      {/* <Checkbox
+        {...label}
+        icon={<BookmarkBorderIcon />} */}
+      {/* /> */}
         </>
         
     )
